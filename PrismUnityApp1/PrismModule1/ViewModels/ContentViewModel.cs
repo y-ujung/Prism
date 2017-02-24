760a4bed1cd5f0c7dba699d46900f16c1272a0cd
yoojung <younyj9404@gmail.com>
2017-02-24 10:52:51 +0900

    resize

--- PrismUnityApp1/PrismModule1/ViewModels/ContentViewModel.cs
+++ PrismUnityApp1/PrismModule1/ViewModels/ContentViewModel.cs
@@ -1,16 +1,139 @@
-﻿using Prism.Commands;
+﻿using ImageProcessor;
+using ImageProcessor.Imaging;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace PrismModule1.ViewModels
 {
     public class ContentViewModel : BindableBase
     {
+        System.Drawing.Point min;
+        System.Drawing.Point max;
+
+        private string savePath;
+        public string SavePath
+        {
+            get { return savePath; }
+            set { SetProperty(ref savePath, value); }
+        }
+
+        private string loadPath;
+        public string LoadPath
+        {
+            get { return loadPath; }
+            set { SetProperty(ref loadPath, value); }
+        }
+
+        private byte[] input;
+        public byte[] Input
+        {
+            get { return input; }
+            set { SetProperty(ref input, value); }
+        }
+
+        private byte[] output;
+        public byte[] Output
+        {
+            get { return output; }
+            set { SetProperty(ref output, value); }
+        }
+
+        private MemoryStream process;
+        public MemoryStream Process
+        {
+            get { return process; }
+            set { SetProperty(ref process, value); }
+        }
+
+        public ICommand LoadImageCommand { get; set; }
+        public ICommand SaveImageCommand { get; set; }
+        public ICommand ResizeImageCommand { get; set; }
+        public ICommand CropImageCommand { get; set; }
+
         public ContentViewModel()
         {
-            var imageFactory = new Image
+            LoadPath = "C:\\Users\\NSTL_DH\\Desktop\\image1.png";
+
+            LoadImageCommand = new DelegateCommand<object>(LoadImage);
+            SaveImageCommand = new DelegateCommand<object>(SaveImage);
+            ResizeImageCommand = new DelegateCommand<object>(ResizeImage);
+            CropImageCommand = new DelegateCommand<object>(CropImage);
+        }
+
+        private void LoadImage(object obj)
+        {
+            Input = File.ReadAllBytes(LoadPath);
+            SavePath = "C:\\Users\\NSTL_DH\\Desktop\\123.png";
+        }
+
+        private void SaveImage(object obj)
+        {
+            using (var imageFactory = new ImageFactory(preserveExifData: true))
+            {
+                imageFactory.Load(Output)
+                    .Save(SavePath);
+            }
+
+        }
+
+        private void ResizeImage(object obj)
+        {
+            var size = new System.Drawing.Size(1000, 1000);
+            using (Process = new MemoryStream())
+            {
+                using (var imageFactory = new ImageFactory(preserveExifData: true))
+                {
+                    imageFactory.Load(Input)
+                        .Resize(size)
+                        .Save(Process);
+                }
+                Output = Process.ToArray();
+            }
+        }
+
+        private void CropImage(object obj)
+        {
+            CropTransparency();
+            //var crop = new ImageProcessor.Imaging.CropLayer(0, 0, 100, 50, CropMode.Percentage);
+            var crop = new ImageProcessor.Imaging.CropLayer(0, 0, 150, 80, CropMode.Pixels);
+            //var crop = new ImageProcessor.Imaging.CropLayer(min.X, min.Y, max.X, max.Y, CropMode.Pixels);
+            using (Process = new MemoryStream())
+            {
+                using (var imageFactory = new ImageFactory(preserveExifData: true))
+                {
+                    imageFactory.Load(Input)
+                        .Crop(crop)
+                        .Save(Process);
+                }
+                Output = Process.ToArray();
+            }
+
+        }
+
+        private void CropTransparency()
+        {
+            Bitmap img = new Bitmap(LoadPath);
+            max = new System.Drawing.Point(img.Width, img.Height);
+            min = new System.Drawing.Point(0, 0);
+
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    System.Drawing.Color col = img.GetPixel(x, y);
+                    if (col.A != 0)
+                        break;
+                }
+            }
         }
     }
 }
+
